68f741d135ff83401365e6f4622b472bc0ff0968
SaiSumakar <saisumakar05082005@gmail.com>
2026-08-11 23:55:51 +0530

    settings logic files setup

---
+++ db/repositories/settingsRepository.ts
@@ -0,0 +1,5 @@
+import { BaseRepository } from "./baseRepository";
+
+class SettingsRepository extends BaseRepository {
+  
+}
---
+++ types/settings.ts
@@ -0,0 +1,7 @@
+export interface Settings {
+
+}
+
+export interface updateSettingsDTO {
+  
+}
